b25324a0008aace7feaa0c5d6c20e82bcecc4c5a
oohyj <hyj0660@gmail.com>
2023-04-10 14:06:13 +0900

    week1_study

---
+++ src/main/java/hello/hellospring/controller/MemberCotroller.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberCotroller {
+
+    private final MemberService memberService ;
+
+    @Autowired
+    public MemberCotroller(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+}
