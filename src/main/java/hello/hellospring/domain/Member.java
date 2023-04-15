c6330cfdeabab1fbc43bdfdbde82e6664b1fb96f
oohyj <hyj0660@gmail.com>
2023-04-15 21:52:39 +0900

    week_2

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.hellospring.domain;
 
-public class Member {
 
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;//시스템이 저장하는 아이디
+
     private String name;
 
     public Long getId() {
