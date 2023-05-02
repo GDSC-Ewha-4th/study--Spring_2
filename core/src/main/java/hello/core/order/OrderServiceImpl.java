e5d654a73071ed9c203a6c75ac1a1e5f418c0dc3
chhaewxn <chaewon1019@ewhain.net>
2023-05-02 17:53:06 +0900

    ✨ feat: Session2 코드 실습

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository = new
+            MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 회원정보 먼저 조회하고
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인 정책에 넘기기
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
