bdf7929426402c6f958b3df927c9a47b9b51ce05
chhaewxn <chaewon1019@ewhain.net>
2023-05-09 15:30:25 +0900

    ✨ feat: Session3-3 코드 실습

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,11 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements MemberService {
+public class MemberServiceImpl implements MemberService { //오직 MemberRepository 인터페이스만 의존!
+    private final MemberRepository memberRepository; // 추상화에만 의존
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository() {
-    };
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
