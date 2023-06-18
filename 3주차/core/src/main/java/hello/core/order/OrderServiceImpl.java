79a36e82808c78b2a5d50e7c2b877f36fd1a20ee
oohyj <hyj0660@gmail.com>
2023-06-18 19:35:24 +0900

    week4

--- 3주차/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 3주차/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,22 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
+    //구체적인 클래스에 대해서 OrderServiceImpl은 전혀 모름
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy  discountPolicy;
+    //private final DiscountPolicy  discountPolicy = new RateDiscountPolicy();
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy  discountPolicy = new FixDiscountPolicy();
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     //주문 생성 요청이 오면
     @Override
--- 3주차/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ 3주차/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,12 +1,20 @@
 package hello.core.member;
 
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
+    MemberService memberService;
 
-   MemberService memberService = new MemberServiceImpl();
+    @BeforeEach //테스트가 실행하기 전에 무조건 실행이 되는
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
 
     @Test
     void join(){
